76aaf640a72e4abbe320250352753a2b60577373
santhiveerapandi <santhiveerapandi@gmail.com>
2023-12-19 20:26:27 +0530

    first commit

--- Controllers/StudentDetailsController.cs
+++ Controllers/StudentDetailsController.cs
@@ -21,4 +21,77 @@ public class StudentController : ControllerBase
     public async Task<ActionResult<IEnumerable<StudentDetail>>> GetStudentDetails() {
         return await _context.StudentDetails.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<StudentDetail>> GetStudentDetail(int id)
+    {
+        var article = await _context.StudentDetails.FindAsync(id);
+
+        if (article == null)
+        {
+            return NotFound();
+        }
+
+        return article;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<StudentDetail>> PostStudentDetail(StudentDetail stu)
+    {
+        _context.StudentDetails.Add(stu);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetStudentDetail", new { id = stu.ID }, stu);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutStudentDetail(int id, StudentDetail stu)
+    {
+        if (id != stu.ID)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(stu).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!StudentDetailExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        // return NoContent();
+        return Ok(stu);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteStudentDetail(int id)
+    {
+        var stu = await _context.StudentDetails.FindAsync(id);
+        if (stu == null)
+        {
+            return NotFound();
+        }
+
+        _context.StudentDetails.Remove(stu);
+        await _context.SaveChangesAsync();
+
+        // return NoContent();
+        return Ok("Successfully Deleted");
+    }
+
+    private bool StudentDetailExists(int id)
+    {
+        return _context.StudentDetails.Any(e => e.ID == id);
+    }
 }
